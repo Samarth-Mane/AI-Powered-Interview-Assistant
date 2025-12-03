1496d95f93112b3b4647c8b1f1ab50c1a0ca54cf
Samarth-Mane <Samarthsmane004@gmail.com>
2025-12-04 01:16:05 +0530

    Refactor nextConfig to include ESLint settings

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-    /* config options here */
-    // images: {
-    //   remotePatterns: [
-    //     {
-    //       protocol: "https",
-    //       hostname: "ik.imagekit.io",
-    //       port: "",
-    //     },
-    //   ],
-    // },
-    typescript: {
-        ignoreBuildErrors: true,
-    },
+const nextConfig: NextConfig & {
+  eslint: { ignoreDuringBuilds: boolean };
+  typescript: { ignoreBuildErrors: boolean };
+} = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
